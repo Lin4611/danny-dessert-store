3b693ff0404eff79a40d33205b2b5a65ed5ffba3
Lin <tzu.hsun.lin046@gmail.com>
2025-11-04 17:40:47 +0800

    update: adjust rwd layout

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -8,26 +8,28 @@ const iconlist = [
 const AppHeader = () => {
   return (
     <>
-      <header className="max-w-[1920px] w-full px-20 pt-15 bg-[#0037C0] lg:flex justify-between items-center">
-        <img
-          src={logo}
-          alt="logo"
-          className="max-w-[396px] w-full h-full object-cover"
-        />
-        <div className="hidden lg:flex max-w-[177px] justify-between items-center">
-          {iconlist.map((p) => (
-            <button
-              type="button"
-              className="w-[70px] h-[70px] flex justify-center items-center"
-            >
-              <img
-                src={p.imgUrl}
-                alt={p.name}
-                className="w-9 h-9 aspect-square object-cover"
-              />
-            </button>
-          ))}
-        </div>
+      <header className="max-w-[1920px] w-full px-20 pt-15 bg-[#0037C0]">
+        <section className="hidden md:flex lg:flex justify-between items-center">
+          <img
+            src={logo}
+            alt="logo"
+            className="max-w-[396px] w-full h-full object-cover"
+          />
+          <div className="flex max-w-[177px] w-full justify-between items-center">
+            {iconlist.map((p) => (
+              <button
+                type="button"
+                className="w-[70px] h-[70px] flex justify-center items-center hover:rounded-full hover:shadow-2xl hover:shadow-[#F7F3D6]"
+              >
+                <img
+                  src={p.imgUrl}
+                  alt={p.name}
+                  className="w-9 h-9 aspect-square object-cover"
+                />
+              </button>
+            ))}
+          </div>
+        </section>
       </header>
     </>
   );
